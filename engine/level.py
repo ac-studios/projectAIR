9b9fcade37de28cbbd7ae11c7f6739241bbcf3ca
obscuredc <austinberm@gmail.com>
2022-01-25 16:02:25 -0500

    initial commits

---
+++ engine/level.py
@@ -0,0 +1,7 @@
+# level.py
+# this file contains level-related functions, and
+# the class definition of the level itself.
+
+class Level:
+    def __init__():
+        pass
---
+++ index.py
@@ -0,0 +1,1 @@
+# this file is all we should have to run to begin the game. it starts up the game.
