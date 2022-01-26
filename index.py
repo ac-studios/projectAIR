cea095d66cb5cac2da15b5420f231dc44eb227b9
obscuredc <austinberm@gmail.com>
2022-01-26 17:30:22 -0500

    switched to pyglet

--- index.py
+++ index.py
@@ -1,98 +1,21 @@
 # this file is all we should have to run to begin the game. it starts up the game.
-import arcade
-from engine.renderer import *
+import pyglet
+from pyglet.window import key
 
-SCREEN_WIDTH, SCREEN_HEIGHT = arcade.window_commands.get_display_size()
-SCREEN_TITLE = "projectAIR"
+window = pyglet.window.Window(fullscreen=True)
 
-# main window
-class projectAIR(arcade.Window):
-    def __init__(self, width, height, title):
-        # enable fullscreen and builtins from arcade
-        super().__init__(width, height, title, fullscreen=True)
+label = pyglet.text.Label(text = "",font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//2,
+                          anchor_x='center', anchor_y='center')
 
-        # set background color to white
-        arcade.set_background_color(arcade.color.WHITE)
+@window.event 
+def on_draw():
+    window.clear()
+    label.draw()
 
-        # If you have sprite lists, you should create them here,
-        # and set them to None
+@window.event  
+def on_key_press(symbol, mod):
+    label.text += chr(symbol)
 
-        # here is the instance for the renderer we need
-        self._instance = RendererInstance()
-
-    def setup(self):
-        """ Set up the game variables. Call to re-start the game. """
-        # Create your sprites and sprite lists here
-        self.sprites = arcade.SpriteList()
-
-        self._instance.append(Image("assets/ac-studios.png", 64, 64, 64, 64))
-
-    def on_draw(self):
-        """
-        Render the screen.
-        """
-
-        # This command should happen before we start drawing. It will clear
-        # the screen to the background color, and erase what we drew last frame.
-        arcade.start_render()
-
-        append2all(self.sprites, arcade, self._instance)
-
-        self.sprites.draw()
-
-        # Call draw() on all your sprite lists below
-
-    def on_update(self, delta_time):
-        """
-        All the logic to move, and the game logic goes here.
-        Normally, you'll call update() on the sprite lists that
-        need it.
-        """
-        pass
-
-    def on_key_press(self, key, key_modifiers):
-        """
-        Called whenever a key on the keyboard is pressed.
-
-        For a full list of keys, see:
-        https://api.arcade.academy/en/latest/arcade.key.html
-        """
-        pass
-
-    def on_key_release(self, key, key_modifiers):
-        """
-        Called whenever the user lets off a previously pressed key.
-        """
-        pass
-
-    def on_mouse_motion(self, x, y, delta_x, delta_y):
-        """
-        Called whenever the mouse moves.
-        """
-        pass
-
-    def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
-        pass
-
-    def on_mouse_release(self, x, y, button, key_modifiers):
-        """
-        Called when a user releases a mouse button.
-        """
-        pass
-
-
-def main():
-    # create an instance of the main class
-    game = projectAIR(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    # begin setup
-    game.setup()
-    # run it
-    arcade.run()
-
-
-if __name__ == "__main__":
-    # execute
-    main()
+pyglet.app.run()
