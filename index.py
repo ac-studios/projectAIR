cd3e647b30a3c9e06d5698f1e7e2d6ecc5db86ab
obscuredc <austinberm@gmail.com>
2022-01-26 19:00:55 -0500

    q

--- index.py
+++ index.py
@@ -4,18 +4,32 @@ from pyglet.window import key
 
 window = pyglet.window.Window(fullscreen=True)
 
-label = pyglet.text.Label(text = "",font_name='Times New Roman',
-                          font_size=36,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
+# intial setup
+
+centerx, centery = window.get_size()[0] // 2, window.get_size()[1] // 2
+topx, topy = window.get_size()[0], window.get_size()[1]
+
+titleScreenImage = pyglet.resource.image("assets/ac-studios.png")
+
+pyglet.font.load(name="monospace")
+
+mouseLabel = pyglet.text.Label("", x=0, y=topy-24, font_size=18, font_name="monospace")
+
+# event handlers
 
 @window.event 
 def on_draw():
     window.clear()
-    label.draw()
+    titleScreenImage.blit(centerx -64, centery -64)
+    mouseLabel.draw()
 
 @window.event  
 def on_key_press(symbol, mod):
-    label.text += chr(symbol)
+    if symbol == key.ESCAPE:
+        pyglet.app.exit()
+
+@window.event  
+def on_mouse_motion(x, y, dx, dy):
+    mouseLabel.text = "[mouse x:"+str(x)+",y:"+str(y)+"] press esc to exit"
 
 pyglet.app.run()
