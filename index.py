1e8f97470d13203a9207520b26d18661d3d845c1
baketsuu <blackburnco@northvilleschools.net>
2022-01-30 20:43:42 -0500

    Quick check

--- index.py
+++ index.py
@@ -1,51 +1,20 @@
-# this file is all we should have to run to begin the game. it starts up the game.
 import pyglet
-from pyglet.window import key
-import math
+from pyglet.window.key import *
+
 
 window = pyglet.window.Window(fullscreen=True)
 
-# intial setup
-
-centerx, centery = window.get_size()[0] // 2, window.get_size()[1] // 2
-topx, topy = window.get_size()[0], window.get_size()[1]
-
-titleScreenImage = pyglet.resource.image("assets/ac-studios.png")
-
-pyglet.font.add_file("assets/fonts/static/RobotoMono-Light.ttf")
-fontRobotoMono = pyglet.font.load(name="Roboto Mono Light")
-
-mouseLabel = pyglet.text.Label("", x=0, y=topy-24, font_size=18, font_name="Roboto Mono Light")
-fpsLabel = pyglet.text.Label("", x=0, y=topy-52, font_size=18, font_name="Roboto Mono Light")
-
-centerlinex = pyglet.shapes.Line(0, centery, topx, centery)
-centerliney = pyglet.shapes.Line(centerx, topy, centerx, 0)
-
-debug = False
-
-# event handlers
-
-@window.event 
+pyglet.font.add_file('assets/fonts/static/Uni.ttf')
+uni = pyglet.font.load('Uni', 48)
+topx,topy=window.get_size()[0],window.get_size()[1]
+txt_label = pyglet.text.Label('AC Studios Presents', anchor_x='center', anchor_y='center', x = topx//2, y = topy//2, font_size=48, font_name='Uni')
+pause = False
+background = (0,0,0)
+@window.event
 def on_draw():
+    pyglet.gl.glClearColor(*background, 1)
     window.clear()
-    titleScreenImage.blit(centerx, centery)
-    if(debug == True):
-        mouseLabel.draw()
-        centerlinex.draw()
-        centerliney.draw()
-        fpsLabel.text = "[fps:"+str(math.floor(pyglet.clock.get_fps()))+"]"
-        fpsLabel.draw()  
 
-@window.event  
-def on_key_press(symbol, mod):
-    if symbol == key.ESCAPE:
-        pyglet.app.exit()
-    if symbol == key.F3:
-        global debug
-        debug = not(debug)
-
-@window.event  
-def on_mouse_motion(x, y, dx, dy):
-    mouseLabel.text = "[mouse x:"+str(x)+",y:"+str(y)+"] f3->debug esc->exit"
-
+    txt_label.draw()
 pyglet.app.run()
+
